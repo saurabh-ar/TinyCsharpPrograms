a13fc4477a776a0eaa6b933a5198be9e85b097bf
saurabh-ar <saurabh.ar02@gmail.com>
2022-12-11 22:53:43 +0530

    GreetFriends-Strings-Program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,10 +4,16 @@ namespace HelloWorld
 {
     internal class Program
     {
+     
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Saurabh, Welcome to the world of Csharp Programming!");
+            Console.WriteLine(GreetFriends("Sameer"));
+
+        }
+
+        public static string GreetFriends(string friend)
+        {
+            return String.Format("Hello {0} my Friend!", friend);
         }
     }
 }
