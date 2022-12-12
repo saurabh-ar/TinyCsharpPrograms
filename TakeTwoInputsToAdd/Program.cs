21f6acb1fb7eda84d82eda52ece16b4207a3e37e
saurabh-ar <saurabh.ar02@gmail.com>
2022-12-12 23:30:55 +0530

    Project to add two user Inputs and return their sum

---
+++ TakeTwoInputsToAdd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TakeTwoInputsToAdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstNumberInput, secondNumberInput;
+            Console.WriteLine("Enter First Number");
+            firstNumberInput = Console.ReadLine();
+
+            Console.WriteLine("Enter Second Number");
+            secondNumberInput = Console.ReadLine();
+            Console.WriteLine("Sum of the Numbers is");
+
+            Console.WriteLine(Sum(firstNumberInput, secondNumberInput));
+        }
+        public static int Sum(string num1, string num2) { 
+            int number1 = int.Parse(num1);
+            int number2 = int.Parse(num2);
+            return number1+ number2;
+        }
+    }
+}
