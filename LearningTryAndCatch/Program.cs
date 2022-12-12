2c017f99b6d07df1fe41b106c8801b527fa6cc24
saurabh-ar <saurabh.ar02@gmail.com>
2022-12-13 00:31:46 +0530

    Learning Try Catch

---
+++ LearningTryAndCatch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LearningTryAndCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Learn Try Catch Finally!");
+            Console.WriteLine("Please enter the input.");
+
+            string userStringInput = Console.ReadLine();
+
+            try
+            {
+                int userIntInput = int.Parse(userStringInput);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Value too large or too large Excpetion");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("General Excpetion");
+            }
+            finally
+            {
+                // Generally used to close File/DB connection etc
+                Console.WriteLine("This will run no matter what");
+            }
+
+            // Prevent window from closing
+            Console.ReadKey();
+        }
+    }
+}
