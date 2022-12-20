a288d317c16f8aaf461095b0b1827cd270e636c5
saurabh-ar <saurabh.ar02@gmail.com>
2022-12-20 21:40:54 +0530

    Added if else some decision making module programs

---
+++ DecisionMaking/DecisionMaking/SimpleIfConditions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SimpleIfConditions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            string userInput = "";
+
+            userInput = Console.ReadLine();
+
+            try
+            {
+                i = int.Parse(userInput);
+                if (i < 30)
+                {
+                    Console.WriteLine("its less than 30 degree celcius");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DecisionMaking/DecisionMaking/UsingTryParse/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UsingTryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int parsedValue;
+            int number;
+
+
+            Console.WriteLine("Enter a Value to Parse as Int :");
+            userInput= Console.ReadLine();
+
+            int.TryParse(userInput, out parsedValue);
+            Console.WriteLine(parsedValue);
+            Console.WriteLine("Parsed Value Type" + parsedValue.GetType());
+            Console.WriteLine(int.TryParse(userInput, out parsedValue));
+            while (userInput == "" || userInput == null)
+            {
+                Console.WriteLine("Please Enter a VALID Value to Parse as Int :");
+                userInput = Console.ReadLine();
+            }
+
+            if (int.TryParse(userInput, out parsedValue))
+            {
+                number = parsedValue;
+                Console.WriteLine(number + " <-- is the Parsed Value.");
+            }
+            else
+            {
+                number = 0;
+                Console.WriteLine("You donot have entered a value has 0 is getting assigned");
+            }
+
+        }
+    }
+}
