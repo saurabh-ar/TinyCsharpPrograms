abbd3c2d43d6b5697c6f68c837293824d8978f26
saurabh-ar <saurabh.ar02@gmail.com>
2022-12-20 23:43:42 +0530

    adding average calculator using loops

---
+++ Loops/AvgOfStudents/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AvgOfStudents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentMarksInput;
+            int parsedInput;
+            int totalMarks = 0; ;
+            int i = 1;
+            do
+            {
+                Console.WriteLine("Enter the marks of your subject : " + i);
+                studentMarksInput = Console.ReadLine();
+                if (int.TryParse(studentMarksInput, out parsedInput)){
+                    Console.WriteLine("Marks entered by student is : " + parsedInput);
+                    totalMarks = totalMarks + parsedInput;
+                    Console.WriteLine("total Marks until now is " + totalMarks);
+                    Console.WriteLine("---------");
+                }
+                i++;
+            } while (i < 5);
+
+            Console.WriteLine("Final Average Marks are : " + totalMarks/5);
+
+          Console.ReadLine();
+        }
+    }
+}
