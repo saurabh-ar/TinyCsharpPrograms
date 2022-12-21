fec0ea0edee44e717e0ffaf935e1fb1538baaa3d
saurabh-ar <saurabh.ar02@gmail.com>
2022-12-21 20:25:29 +0530

    first Class intial commit

---
+++ OOPs/myFirstClass/Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myFirstClass
+{
+    internal class Human
+    {
+        //member properties
+        public string name;
+        public int age;
+
+        //methods
+        public void speak(string word)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
---
+++ OOPs/myFirstClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace myFirstClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human saurabh = new Human();
+            saurabh.age = 32;
+            saurabh.name = "A R Saurabh";
+
+            saurabh.speak("Hey there, Whats up?");
+        }
+    }
+}
