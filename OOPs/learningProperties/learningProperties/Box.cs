58ce35286d8f7881f65984905283dfb06ff55df0
saurabh-ar <saurabh.ar02@gmail.com>
2022-12-23 08:42:53 +0530

    Hands on with Properties Implementation

---
+++ OOPs/learningProperties/learningProperties/Box.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningProperties
+{
+    class Box
+    {
+        // member variables
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Volume
+        {
+            get
+            {
+                return this.Length * this.Width * this.Height;
+            }
+        }
+        public int FrontSurface
+        {
+            get
+            {
+                return this.Length * this.Width;
+            }
+        }
+
+        // constructor
+
+        public Box(int length, int width, int height)
+        {
+            if ((length > 0) && (width > 0) && (height > 0))
+            {
+                this.Length = length;
+                this.Width = width;
+                this.Height = height;
+            }
+
+        }
+
+        // member methods
+        public void displayInfo()
+        {
+            Console.WriteLine("Here are the dimensions");
+            Console.WriteLine($"Length is :{Length}");
+            Console.WriteLine($"Width is :{Width}");
+            Console.WriteLine($"Height is :{Height}");
+            Console.WriteLine($"Volume is :{Volume}");
+            Console.WriteLine($"FrontSurface is :{FrontSurface}");
+        }
+
+    }
+}
---
+++ OOPs/learningProperties/learningProperties/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace learningProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(3,4,5);
+
+            box.displayInfo();
+            Console.ReadLine();
+        }
+    }
+}
