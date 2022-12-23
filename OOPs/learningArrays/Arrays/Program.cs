56b4515e216c898a3a4919f87927783d6f79284b
saurabh-ar <saurabh.ar02@gmail.com>
2022-12-23 21:55:57 +0530

    Demonstrating first array program

---
+++ OOPs/learningArrays/Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // array declaration
+            int i = 0;
+            int parsedRes=0;
+            string userInput;
+            int[] grades = new int[5];
+            while (i < grades.Length)
+            {
+                Console.WriteLine($"Please Enter a Value for Grade {i} :");
+                
+                try
+                {
+                    userInput  = Console.ReadLine();
+                    
+                    if (userInput != null)
+                    {
+                        parsedRes = Convert.ToInt32(userInput);
+                    }
+                    grades[i] = parsedRes;
+                    i++;
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Terminating the program.");
+                    break;
+                }
+
+            }
+            i = 0;
+            if (grades[3] > 4)
+            {
+                while (i < grades.Length)
+                {
+                    Console.WriteLine($"grade value {i} : {grades[i]}");
+                    i++;
+                }
+            }
+
+        }
+    }
+}
