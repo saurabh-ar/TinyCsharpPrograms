59bb915c99b5d0e25c70e03ae102adca57e16009
saurabh-ar <saurabh.ar02@gmail.com>
2022-12-24 12:24:55 +0530

    Learing ForEach first commit

---
+++ OOPs/learningForEach/ForEachLoops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ForEachLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array Declaration & Intialization
+            string[] arr = new string[5];
+
+            // Wrong Array Intialisation
+            // arr = { "Me","Me","Me","Me","Me"}
+
+            // Correct Way : You can declare an array without intializing it.
+            // But you must new keyword to when you assign a new array to it.
+            arr = new string[] {"saurabh", "I", "me", "myself", "rajiv"};
+
+            // Loop to all of the array elements
+            // ForEach Loop
+            foreach (var item in arr)
+            {
+                Console.WriteLine($"Hello there {item.ToUpper()}, Welcome to Learn C#");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
