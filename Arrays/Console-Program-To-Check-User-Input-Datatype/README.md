471b8b9899394a1d4f7d9bf161a91839d72a8cfd
saurabh-ar <saurabh.ar02@gmail.com>
2022-12-24 20:50:07 +0530

    Check user datatype program with C# added

---
+++ Arrays/Console-Program-To-Check-User-Input-Datatype/README.md
@@ -0,0 +1,45 @@
+##User Input Datatype Validation in C Sharp
+
+Create an application that takes 2 input values.
+
+> 1.  Any input value (1st input)
+> 2.  Asks the data type of the input value. (2nd input)
+
+It will print to the console the options like below to take input for the 2nd input value:
+
+> Press 1 for String
+> Press 2 for integer
+> Press 3 for Boolean
+    
+
+If the input value for the 2nd input is 1 then the application should check that if the entered 1st input is a valid string or not. Hereby we even want to check, if it is a complete alphabetic entry (so no numbers accepted)
+
+If the input value for the 2nd input is 2 then the application should check if the entered 1st input is a valid integer or not.
+
+Based on the input the 1st value and the selection of data type using the 2nd input, the application should return whether the entered 1st value is of data type selected by the user or not.
+
+Please make sure to use a switch statement. To check the valid string you can write your custom logic.
+
+_For example:_
+
+_Enter a value: {here you can enter any value}_
+
+_Select the Data type to validate the input you have entered._
+
+_Press 1 for String_
+
+_Press 2 for Integer_
+
+_Press 3 for Boolean_
+
+_Here, if you enter 1 it should return below message_
+
+_You have entered a value: Denis Panjuta_
+
+_It is a valid: String_
+
+_Here, if you enter 2 it should return below message_
+
+_You have entered a value: Denis Panjuta_
+
+_It is an invalid: Integer_
---
+++ Arrays/Console-Program-To-Check-User-Input-Datatype/validateUserInputDatatype/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace validateUserInputDatatype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userDatatypeInputToInt = 0;
+
+            Console.WriteLine("Enter a value: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Select the Data type to validate the input you have entered.");
+            Console.WriteLine("Press 1 for String\nPress 2 for Integer\nPress 3 for Boolean");
+            Console.WriteLine("Enter your response below : ");
+            string userDatatypeInput = Console.ReadLine();
+
+            if (int.Parse(userDatatypeInput) == 1)
+            {
+                userDatatypeInputToInt = 1;
+            }
+            else if (int.Parse(userDatatypeInput) == 2)
+            {
+                userDatatypeInputToInt = 2;
+            }
+            else if (int.Parse(userDatatypeInput) == 3)
+            {
+                userDatatypeInputToInt = 3;
+            }
+
+            switch (userDatatypeInputToInt)
+            {
+                case 2:
+                    Console.WriteLine("You have entered a value: {0}", userInput);
+                    if (int.TryParse(userInput, out _))
+                    {
+                        Console.WriteLine("It is a valid: Integer");
+                    }
+                    else
+                    {
+                        Console.WriteLine("It is a invalid: Integer");
+                    }
+                    break;
+
+                case 3:
+                    Console.WriteLine("You have entered a value: {0}", userInput);
+                    if (bool.TryParse(userInput, out _))
+                        Console.WriteLine("It is a valid: Boolean");
+                    else
+                        Console.WriteLine("It is a invalid: Boolean");
+                    break;
+                // Since you cannot parse to string...if the previous statements came up false -> IT's STRING Type.
+                case 1:
+                    Console.WriteLine("You have entered a value: {0}", userInput);
+                    if ((!bool.TryParse(userInput, out _)) && (!int.TryParse(userInput, out _)))
+                    {
+                        Console.WriteLine("It is a valid: String");
+                    }
+                    else
+                    {
+                        Console.WriteLine("It is a invalid: String");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
