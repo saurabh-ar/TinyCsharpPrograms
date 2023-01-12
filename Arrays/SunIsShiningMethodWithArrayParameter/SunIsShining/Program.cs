c8c2fe4fc6591a8e79c00e69f29b4da1ca6f6c4f
saurabh-ar <saurabh.ar02@gmail.com>
2023-01-12 08:53:48 +0530

    added a method with an array as parameter

---
+++ Arrays/SunIsShiningMethodWithArrayParameter/SunIsShining/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SunIsShining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] happiness = new int[] { 34, 76, 56, 89, 100 };
+            foreach(int value in happiness)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine("------after------");
+            SunIsShining(happiness);
+            foreach (int value in happiness)
+            {
+                Console.WriteLine(value);
+            }
+        }
+        static int[] SunIsShining(int[] happyValues)
+        {
+            for(int i = 0; i < happyValues.Length; i++)
+            {
+                happyValues[i] += 2;
+            }
+            return happyValues;
+        }
+    }
+}
