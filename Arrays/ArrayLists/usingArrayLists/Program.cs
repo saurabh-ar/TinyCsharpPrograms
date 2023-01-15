f84b17e7b82648809bba136de2c61bd1e17ac465
saurabh-ar <saurabh.ar02@gmail.com>
2023-01-15 10:17:13 +0530

    using arraylist

---
+++ Arrays/ArrayLists/usingArrayLists/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace usingArrayLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myArrayList = new ArrayList();
+            myArrayList.Add(12);
+            myArrayList.Add(12.22);
+            myArrayList.Add(1123.662);
+            myArrayList.Add(45);
+            myArrayList.Add(16);
+            myArrayList.Add("Hello there");
+            myArrayList.Add("Print these lines");
+            myArrayList.Add("12345");
+            myArrayList.Add("XX YY ZZ");
+
+            double sum = 0;
+            foreach(object obj in myArrayList)
+            {
+                if(obj is int)
+                {
+                    sum = sum + Convert.ToInt16(obj);
+                }
+                if(obj is double)
+                {
+                    sum = sum + Convert.ToDouble(obj);
+                }
+                if(obj is string)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
